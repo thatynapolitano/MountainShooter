3232536aa8bd583fd3e7f8e867dd17da16af2eb8
Thatiana Napolitano <thatiananapolitano@gmail.com>
2024-11-07 21:15:38 -0300

    adicionando nome e RU

--- code/Menu.py
+++ code/Menu.py
@@ -5,7 +5,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_WHITE, C_YELLOW, C_BLACK
 
 
 class Menu:
@@ -23,6 +23,7 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(30, "Thatiana de Assis Napolitano / RU: 4302056", C_BLACK, ((WIN_WIDTH / 2), 350))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
